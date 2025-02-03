0f08931068d00a8b444e71230b84ec5575e838e7
Dip1435 <pateldip1436@gmail.com>
2025-02-03 15:07:38 +0530

    updated scripts

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  basePath: '/landing-page',
-  assetPrefix: '/landing-page/',
-  output: 'export'
- 
 };
 
 export default nextConfig;
